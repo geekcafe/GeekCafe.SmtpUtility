e838fc776262524f5a8aed8d394227c76b9faae0
Eric Wilson <eric.wilson@geekcafe.com>
2022-02-19 14:10:53 -0500

    🚀 feat (SmtpUtility Interface): init

---
+++ src/GeekCafe.SmtpUtility.Interfaces/ISmtpService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Net.Mail;
+using System.Threading.Tasks;
+
+namespace GeekCafe.SmtpUtility.Interfaces
+{
+    public interface ISmtpService
+    {
+        Task<bool> SendAsync(string host, int port, bool useSsl, string username,
+            string password, string fromEmailAddress, string fromDisplay, string toEmailAddress,
+            string subject, string body, bool bodyIsHtml,
+            MailAddressCollection cc = null, MailAddressCollection bcc = null,
+            AttachmentCollection attachments = null,
+            bool throwExceptions = false);
+    }
+}
