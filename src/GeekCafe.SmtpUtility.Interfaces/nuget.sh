a0573299ba8500f1750222475960504514cb3e93
Eric Wilson <eric.wilson@geekcafe.com>
2022-02-19 16:28:17 -0500

    🚀 feat (SmtpUtility.Interfaces Nuget): deploys as Nuget package

---
+++ src/GeekCafe.SmtpUtility.Interfaces/nuget.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+nuget_api_key=""
+
+if [ -z "${1}" ]    
+then
+
+      echo "you need to supply your api key as the first parameter"
+      exit -1
+else
+    
+    nuget_api_key="${1}"
+
+    echo "api key found."
+    # todo auto increment
+    # build it
+    dotnet build --configuration Release
+
+    # pack it
+    dotnet pack --configuration Release
+
+    # publish it
+    # skip duplicates
+    # todo get the version number automatically
+    dotnet nuget push bin/Release/GeekCafe.SmtpUtility.Interfaces.1.0.0.nupkg  -k "${nuget_api_key}"  -s https://api.nuget.org/v3/index.json --skip-duplicate
+fi
+
+
+
